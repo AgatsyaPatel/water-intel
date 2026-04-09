6747c2801bfd1f9630deadcfff261fb2caa722a0
AgatsyaPatel <imagatsya@gmail.com>
2026-04-09 18:21:07 -0400

    add all utilities export and US states filter

--- Report.py
+++ Report.py
@@ -1,24 +1,23 @@
 import pandas as pd
 
 df = pd.read_csv("water_procurement_scores.csv")
-
-#kEEPING ONLY THE COLUMNS WE NEED
-df = df[["PWS_NAME", "STATE_CODE", "SYSTEM_AGE_YEARS", "POPULATION_SERVED_COUNT", "PROCUREMENT_SCORE"]]
+df = df[["PWS_NAME", "STATE_CODE", "SYSTEM_AGE_YEARS", "POPULATION_SERVED_COUNT", "PROCUREMENT_SCORE_V2"]]
 
 
-# state summary BEFORE renaming columns
-state_summary = df.groupby("STATE_CODE")["PROCUREMENT_SCORE"].mean().round(1).reset_index()
+# state summary of renaming columns
+US_STATES = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+df = df[df["STATE_CODE"].isin(US_STATES)]
+state_summary = df.groupby("STATE_CODE")["PROCUREMENT_SCORE_V2"].mean().round(1).reset_index()
 state_summary.columns = ["State", "Avg Procurement Score"]
 state_summary = state_summary.sort_values("Avg Procurement Score", ascending=False)
 
-# rename columns so they look clean in Excel
-df.columns = ["Utility Name", "State", "Age (Years)",
+
+# renaming columns for Excel
+df.columns = ["Utility Name", "State", "Age (Years)", 
               "Population Served", "Procurement Score"]
 
 with pd.ExcelWriter("water_intel_report.xlsx", engine="openpyxl") as write:
-    # Sheet 1: Top 100 utilities
-    df.head(100).to_excel(write, sheet_name="Top 100 Utilities", index=False)
-    # Sheet 2: State Summary
+    df.to_excel(write, sheet_name="All Utilities", index=False)
     state_summary.to_excel(write, sheet_name="State Summary", index=False)
 
 print("Done! water_intel_report.xlsx created")
